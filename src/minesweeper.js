76301310d1409c45d419e53d0c188271195b40bb
alphaseinor <alphaseinor@gmail.com>
2017-12-17 19:11:17 -0600

    fixed bug where bomb could be randomly generated on the same position as an existing bomb, added some code for neighbor numbering, not finished yet

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -24,15 +24,39 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
   //random number generator for bomb map
   //control flow bug: more than one bomb can be assigned to the same square
   while (numberOfBombsPlaced < numberOfBombs) {
-
-    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
-    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-    board[randomRowIndex][randomColumnIndex] = 'B';
-    numberOfBombsPlaced++;
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows); //random number for rows
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns); //random number for columns
+    if(board[randomRowIndex][randomColumnIndex] !== 'B'){ //checks to see if there is already a bomb placed
+    board[randomRowIndex][randomColumnIndex] = 'B'; //if bomb is not place then place a bomb
+    numberOfBombsPlaced++; //if bomb is placed increment numberOfBombsPlaced
+    }
   }
-
-
   return board;
+};
+
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) =>{
+  const neighborOffsets = [
+    [-1,-1],
+    [-1,0],
+    [-1,1],
+    [0,-1],
+    [0,1],
+    [1,-1],
+    [1,0],
+    [1,1],
+  ];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+  neighborOffsets.forEach(offset =>{
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[1];
+    if(neighborRowIndex >= 0 && neighborRowIndex < numberOfColumns && neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns){
+      if(bombBoard[neighborRowIndex][] == ){
+
+      }
+    }
+  });
 };
 
 const printBoard = (board) => {
@@ -43,3 +67,4 @@ const printBoard = (board) => {
 const playerBoard = generatePlayerBoard(6,4);
 const bombBoard = generateBombBoard(6,4,4);
 printBoard(playerBoard);
+printBoard(bombBoard);
